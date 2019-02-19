9381c00e8ad6b8a5cb319f0e2933551db755b241
Jimmy Kamau <jimmykambiz@gmail.com>
2019-02-19 16:53:22 +0300

    Initial setup

---
+++ api/olduka/settings/development.py
@@ -0,0 +1,13 @@
+import os
+
+from olduka.settings.base import *
+
+ALLOWED_HOSTS = ['*']
+
+DEBUG = True
+
+SECURE_SSL_REDIRECT = False
+
+CSRF_COOKIE_SECURE = False
+
+SESSION_COOKIE_SECURE = False
