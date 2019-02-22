50fb2098047dd4220cadd287c45b243118374373
Jimmy Kamau <jimmykambiz@gmail.com>
2019-02-22 20:51:32 +0300

    Register v1 urls

---
+++ api/olduka/v1/urls.py
@@ -0,0 +1,10 @@
+from django.urls import include, path
+from rest_framework_swagger.views import get_swagger_view
+
+app_name = 'v1'
+schema_view = get_swagger_view(title='Olduka')
+
+urlpatterns = [
+    path('', schema_view),
+    path('auth/', include('olduka.v1.authentication.urls'))
+]
