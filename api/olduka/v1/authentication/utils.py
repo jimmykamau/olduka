122bd4e4fa8159f03c07dffe8f1cee440bc98231
Jimmy Kamau <jimmykambiz@gmail.com>
2019-03-12 20:10:05 +0300

    Add helper method to change jwt_secret

--- api/olduka/v1/authentication/utils.py
+++ api/olduka/v1/authentication/utils.py
@@ -1,2 +1,10 @@
+import uuid
+
+
 def get_jwt_secret(user):
     return user.user_profile.jwt_secret
+
+
+def change_jwt_secret(user):
+    user.user_profile.jwt_secret = uuid.uuid4()
+    user.user_profile.save()
