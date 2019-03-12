f6c2c3d656aad2692e397d03858ca0cb27abfe67
Jimmy Kamau <jimmykambiz@gmail.com>
2019-03-12 03:26:17 +0300

    Add tests for RetrieveUpdateDestroyUserView

--- api/olduka/v1/authentication/tests/test_views.py
+++ api/olduka/v1/authentication/tests/test_views.py
@@ -1,10 +1,9 @@
+import factory
+import olduka.v1.authentication.models as authentication_models
+import olduka.v1.authentication.tests.factories as authentication_factories
 from django.contrib.auth.models import User
 from django.urls import reverse
 from rest_framework.test import APITestCase
-import factory
-
-import olduka.v1.authentication.models as authentication_models
-import olduka.v1.authentication.tests.factories as authentication_factories
 
 
 class CreateUserViewTests(APITestCase):
@@ -13,16 +12,19 @@ class CreateUserViewTests(APITestCase):
         self.user_profile = authentication_factories.UserProfileFactory.build()
         self.user = self.user_profile.user
         self.user_profile_data = dict(
-            username = self.user.username,
-            first_name = self.user.first_name,
-            last_name = self.user.last_name,
-            email = self.user.email,
-            password = self.user.password,
-            user_profile = {
+            username=self.user.username,
+            first_name=self.user.first_name,
+            last_name=self.user.last_name,
+            email=self.user.email,
+            password=self.user.password,
+            user_profile={
                 "mobile": self.user_profile.mobile
             }
         )
         self.url = reverse('v1:create-user')
+
+    def tearDown(self):
+        self.client.force_authenticate(user=None)
 
     def test_create_user(self):
         response = self.client.post(
@@ -36,6 +38,71 @@ class CreateUserViewTests(APITestCase):
         )
         self.assertTrue(
             authentication_models.UserProfile.objects.filter(
-                mobile=self.user_profile_data.get('user_profile').get('mobile') 
+                mobile=self.user_profile_data.get('user_profile').get('mobile')
             )
         )
+
+
+class RetrieveUpdateDestroyUserViewTests(APITestCase):
+
+    def setUp(self):
+        self.user_profile = authentication_factories.UserProfileFactory.create()
+        self.client.force_authenticate(user=self.user_profile.user)
+        self.url = reverse('v1:user', kwargs={'pk': self.user_profile.user.id})
+
+    def tearDown(self):
+        self.client.force_authenticate(user=None)
+
+    def test_get_user(self):
+        response = self.client.get(self.url, format='json')
+        self.assertEqual(200, response.status_code)
+        self.assertEqual(self.user_profile.user.email, response.data['email'])
+
+    def test_update_user(self):
+        new_user_data = dict(
+            first_name="John",
+            last_name="Doe"
+        )
+        response = self.client.patch(self.url, new_user_data, format='json')
+        self.assertEqual(200, response.status_code)
+        self.assertTrue(
+            User.objects.filter(
+                first_name=new_user_data['first_name'],
+                last_name=new_user_data['last_name']
+            )
+        )
+
+    def test_delete_user(self):
+        self.client.force_authenticate(user=None)
+        user_details = dict(
+            username=self.user_profile.user.username,
+            password="12345"
+        )
+        self.user_profile.user.set_password(user_details['password'])
+        self.user_profile.user.save()
+        auth_token_response = self.client.post(
+            reverse('v1:login'), user_details, format='json'
+        )
+        jwt_token = auth_token_response.data['token']
+        self.client.credentials(HTTP_AUTHORIZATION='JWT {}'.format(jwt_token))
+        response = self.client.delete(self.url, format='json')
+        self.assertEqual(204, response.status_code)
+        # Confirm that the user hasn't actually been deleted
+        self.assertTrue(
+            User.objects.filter(
+                email=self.user_profile.user.email
+            ).exists()
+        )
+        # Check that the user is inactive
+        self.assertFalse(
+            User.objects.get(
+                email=self.user_profile.user.email
+            ).is_active
+        )
+        user_details_response = self.client.get(self.url, format='json')
+        self.assertEqual(401, user_details_response.status_code)
+        # Confirm the user can't log in
+        auth_token_response = self.client.post(
+            reverse('v1:login'), user_details, format='json'
+        )
+        self.assertEqual(400, auth_token_response.status_code)
