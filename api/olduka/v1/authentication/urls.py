8b5db842a41945041140898209246fb7ce14f529
Jimmy Kamau <jimmykambiz@gmail.com>
2019-03-12 03:23:54 +0300

    Add login, token-refresh and user endpoints

--- api/olduka/v1/authentication/urls.py
+++ api/olduka/v1/authentication/urls.py
@@ -1,10 +1,18 @@
-from django.urls import include, path
-
 import olduka.v1.authentication.views.user_views as user_views
+from django.urls import include, path
+from rest_framework_jwt.views import obtain_jwt_token, refresh_jwt_token
 
 urlpatterns = [
+    path('login/', obtain_jwt_token, name='login'),
+    path('token/refresh/', refresh_jwt_token, name='refresh-token'),
     path(
         'user/create/',
         user_views.CreateUserView.as_view(),
-        name='create-user')
+        name='create-user'
+    ),
+    path(
+        'user/<slug:pk>/',
+        user_views.RetrieveUpdateDestroyUserView.as_view(),
+        name='user'
+    )
 ]
