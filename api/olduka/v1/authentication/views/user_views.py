c3d0624a20de877952152ca25bb72a5117fa293f
Jimmy Kamau <jimmykambiz@gmail.com>
2019-03-12 03:25:54 +0300

    Create RetrieveUpdateDestroyUserView

--- api/olduka/v1/authentication/views/user_views.py
+++ api/olduka/v1/authentication/views/user_views.py
@@ -1,6 +1,7 @@
-from rest_framework import generics, permissions
-
+import olduka.v1.authentication.models as authentication_models
 import olduka.v1.authentication.serializers.user_serializers as user_serializers
+from django.contrib.auth.models import User
+from rest_framework import generics, permissions
 
 
 class CreateUserView(generics.CreateAPIView):
@@ -9,3 +10,16 @@ class CreateUserView(generics.CreateAPIView):
     """
     permission_classes = (permissions.AllowAny,)
     serializer_class = user_serializers.UserSerializer
+
+
+class RetrieveUpdateDestroyUserView(generics.RetrieveUpdateDestroyAPIView):
+    """
+    Get, Update and Destroy a User
+    """
+    permission_classes = (permissions.IsAuthenticated,)
+    serializer_class = user_serializers.UserSerializer
+    queryset = User.objects.all()
+
+    def perform_destroy(self, instance):
+        instance.is_active = False
+        instance.save()
