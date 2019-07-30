5370b19d572d26078bfd3978286e0092f6a9d7b4
Jimmy Kamau <jimmykambiz@gmail.com>
2019-07-30 21:39:09 +0300

    Add category and item URLs

--- api/olduka/v1/product/urls.py
+++ api/olduka/v1/product/urls.py
@@ -4,8 +4,28 @@ import olduka.v1.product.views as product_views
 
 urlpatterns = [
     path(
+        'category/',
+        product_views.ListCategoriesView.as_view(),
+        name='list-categories'
+    ),
+    path(
+        'category/<slug:pk>/',
+        product_views.GetCategoryView.as_view(),
+        name='get-category'
+    ),
+    path(
+        'category/<slug:pk>/item/',
+        product_views.GetCategoryItemsView.as_view(),
+        name='get-category-items'
+    ),
+    path(
         'item/',
         product_views.ListItemsView.as_view(),
         name='list-items'
+    ),
+    path(
+        'item/<slug:pk>/',
+        product_views.GetItemView.as_view(),
+        name='get-item'
     )
 ]
