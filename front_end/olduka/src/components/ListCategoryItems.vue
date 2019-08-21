371fc252282fda6935f291822e70528b9bf5a5d5
Jimmy Kamau <jimmykambiz@gmail.com>
2019-08-22 01:56:09 +0300

    Create ListCategoryItems component

---
+++ front_end/olduka/src/components/ListCategoryItems.vue
@@ -0,0 +1,67 @@
+<template>
+  <div class="uk-container">
+    <div v-if="loading" class="uk-flex uk-flex-center uk-flex-middle">
+      <span uk-spinner="ratio: 5"></span>
+    </div>
+    <div v-else>
+      <div uk-grid="masonry: true" class="uk-flex-center">
+        <div class="uk-flex uk-flex-center" v-for="item in categoryItems.category_item" :key="item._id">
+          <div class="uk-card uk-card-hover">
+            <div class="uk-card-media-top">
+              <img :src="item.images[0].image_url">
+            </div>
+            <div class="uk-card-body">
+              <h3 class="uk-card-title">{{ item.name }}</h3>
+              <div v-if="item.price.discount > 0">
+                <p><s><em> KES {{ item.price.price }}</em></s> KES {{ item.price.price - item.price.discount }}</p>
+              </div>
+              <div v-else>
+                <p>KES {{ item.price.price }}</p>
+              </div>
+            </div>
+          </div>
+        </div>
+      </div>
+    </div>
+  </div>
+</template>
+
+<script>
+import { mapGetters } from 'vuex'
+
+export default {
+  data () {
+    return {
+      loading: false
+    }
+  },
+  computed: {
+    ...mapGetters('product', {
+      categoryItems: 'categoryItems'
+    })
+  },
+  created () {
+    this.getItems()
+  },
+  watch: {
+    '$route': 'getItems'
+  },
+  methods: {
+    getItems () {
+      this.loading = true
+      this.$store.dispatch('product/getCategoryItems', this.$route.params.category_id).then(
+        () => {
+          this.loading = false
+        }
+      ).catch(
+        err => {
+          this.$toasted.error(
+            'There was an error fetching items in the category. Kindly try again later'
+          )
+          this.$router.go(-1)
+        }
+      )
+    }
+  }
+}
+</script>
