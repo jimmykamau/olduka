a7ed61f2b288642d5d92eeb7dfb2f1a81f9d0815
Jimmy Kamau <jimmykambiz@gmail.com>
2019-11-05 17:42:28 +0300

    Create sidenav

---
+++ front_end/olduka/src/components/SideNav.vue
@@ -0,0 +1,39 @@
+<template>
+  <div class="tm-sidebar-left uk-visible@m">
+    <ul v-for="category in productCategories" :key="category._id" class="uk-nav uk-nav-default tm-nav">
+      <router-link :to="`/category/${category._id}/items`" class="uk-nav-header" tag="li"><a><b>{{category.name}}</b></a></router-link>                            
+      <router-link v-for="subcategory in category.subcategory" :key="subcategory._id" :to="`/category/${subcategory._id}/items`" tag="li"><a>{{subcategory.name}}</a></router-link>
+    </ul>
+  </div>
+</template>
+
+<script>
+import { mapGetters } from 'vuex'
+
+export default {
+  computed: {
+    ...mapGetters('product', {
+      productCategories: 'productCategories'
+    })
+  },
+  created () {
+    this.getCategories()
+  },
+  methods: {
+    getCategories () {
+      this.$store.dispatch('product/getCategories').then(
+        () => {
+          this.loading = false
+        }
+      ).catch(
+        err => {
+          this.$toasted.error(
+            'There was an error fetching categories. Kindly try again later'
+          )
+          this.$router.go(-1)
+        }
+      )
+    }
+  }
+}
+</script>
