bf8a852b30e83a5ffaa71ad5b6834aa763588cb1
Jimmy Kamau <jimmykambiz@gmail.com>
2019-11-05 17:44:07 +0300

    Create cart functionality

--- front_end/olduka/src/store/index.js
+++ front_end/olduka/src/store/index.js
@@ -2,6 +2,7 @@ import Vue from 'vue'
 import Vuex from 'vuex'
 import auth from './modules/auth'
 import product from './modules/product'
+import cart from './modules/cart'
 
 Vue.use(Vuex)
 
@@ -10,7 +11,8 @@ const debug = process.env.NODE_ENV !== 'production'
 export default new Vuex.Store({
   modules: {
     auth,
-    product
+    product,
+    cart
   },
   strict: debug
 })
