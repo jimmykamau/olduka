2f04db3bd980eb59a1220b300d16377f7c8583fa
Jimmy Kamau <jimmykambiz@gmail.com>
2019-11-05 17:46:15 +0300

    Update layout and add cart functionality

--- front_end/olduka/src/components/SingleItem.vue
+++ front_end/olduka/src/components/SingleItem.vue
@@ -1,33 +1,67 @@
 <template>
-  <div class="uk-container">
+  <div>
     <div v-if="loading" class="uk-flex uk-flex-center uk-flex-middle">
       <span uk-spinner="ratio: 5"></span>
     </div>
-    <div v-else uk-grid class="uk-child-width-expand@s uk-child-width-1-2@m">
-      <div class="uk-position-relative uk-visible-toggle uk-light" tabindex="-1" uk-slideshow>
-        <ul class="uk-slideshow-items">
-          <li v-for="image in currentItem.images" :key="image.image_url">
-            <img :src="image.image_url" :alt="currentItem.name" uk-cover>
-          </li>
-        </ul>
-        <a class="uk-slidenav-large uk-position-center-left uk-position-small" href="#" uk-slidenav-previous uk-slideshow-item="previous"></a>
-        <a class="uk-slidenav-large uk-position-center-right uk-position-small" href="#" uk-slidenav-next uk-slideshow-item="next"></a>
+    <div uk-grid>
+      <div class="uk-width-1-4@m">
+        <sidenav/>
       </div>
-      <div class="uk-padding">
-        <h1 class="uk-heading-medium">{{ currentItem.name }}</h1>
-        <p>{{ currentItem.description }}</p>
-        <div v-if="currentItem.price.discount > 0">
-          <p class="uk-h3"><b><s><em> KES {{ currentItem.price.price }}</em></s> KES {{ currentItem.price.price - currentItem.price.discount }}</b></p>
-        </div>
-        <div v-else>
-          <p class="uk-h3"><b>KES {{ currentItem.price.price }}</b></p>
-        </div>
-        <div v-if="currentItem.quantity <= 10">
-          <p v-if="currentItem.quantity === 0"><em>Out of stock</em></p>
-          <p v-else><em>{{ currentItem.quantity }} left in stock</em></p>
-        </div>
-        <div class="uk-flex uk-flex-center">
-          <button class="uk-button tm-button-large uk-button-primary" style="border-radius: 500px" :disabled="currentItem.quantity === 0">Add to cart</button>
+      <div v-if="!loading" class="uk-width-3-4@m uk-width-expand@s">
+        <div class="uk-container">
+          <div uk-grid>
+            <div class="uk-width-1-1 uk-child-width-expand@s" uk-grid>
+              <div>
+                <div class="uk-position-relative uk-visible-toggle uk-light" tabindex="-1" uk-slideshow="ratio: 300:300">
+                  <ul class="uk-slideshow-items">
+                    <li v-for="image in currentItem.images" :key="image.image_url">
+                      <img :src="image.image_url" :alt="currentItem.name" uk-cover>
+                    </li>
+                  </ul>
+                  <a class="uk-slidenav-large uk-position-center-left uk-position-small" href="#" uk-slidenav-previous uk-slideshow-item="previous"></a>
+                  <a class="uk-slidenav-large uk-position-center-right uk-position-small" href="#" uk-slidenav-next uk-slideshow-item="next"></a>
+                </div>
+              </div>
+              <div class="uk-flex uk-flex-column uk-flex-middle uk-flex-center">
+                <div>
+                  <h1 class="uk-heading-small">{{ currentItem.name }}</h1>
+                </div>
+                <div>
+                  <p class="uk-h3">
+                    <b v-if="currentItem.price.discount > 0">
+                      <s><em> KES {{ currentItem.price.price }}</em></s> KES {{ currentItem.price.price - currentItem.price.discount }}
+                    </b>
+                    <b v-else>KES {{ currentItem.price.price }}</b>
+                  </p>
+                </div>
+                <div v-if="currentItem.quantity <= 10">
+                  <p>
+                    <em v-if="currentItem.quantity === 0">Out of stock</em>
+                    <em v-else>{{ currentItem.quantity }} left in stock</em>
+                  </p>
+                </div>
+                <div class="uk-flex@m uk-flex-middle@m">
+                  <div v-if="isLoggedIn" uk-grid>
+                    <div v-if="currentItem.quantity > 0">
+                      <select class="uk-select uk-form-width-small" v-model="quantity">
+                        <option value="1" disabled>Quantity: 1</option>
+                        <option v-for="i in currentItem.quantity" :value="i" :key="i">{{ i }}</option>
+                      </select>
+                    </div>
+                    <div>
+                      <button v-if="isLoggedIn" class="uk-button tm-button-large uk-button-primary" style="border-radius: 500px" :disabled="currentItem.quantity === 0" @click="addToCart()">Add to cart</button>
+                    </div>
+                  </div>
+                  <div v-else>
+                    <p>Kindly <router-link to="/signup">sign up</router-link>/<router-link :to="{ name: 'login', query: { nextURL: currentRoute } }">log in</router-link> to purchase</p>
+                  </div>
+                </div>
+              </div>
+            </div>
+            <div>
+              <p>{{ currentItem.description }}</p>
+            </div>
+          </div>
         </div>
       </div>
     </div>
@@ -41,12 +75,16 @@ export default {
   data () {
     return {
       loading: false,
-      quantity: 1
+      quantity: 1,
+      currentRoute: this.$route.currentRoute
     }
   },
   computed: {
     ...mapGetters('product', {
       currentItem: 'currentItem'
+    }),
+    ...mapGetters('auth', {
+      isLoggedIn: 'isLoggedIn'
     })
   },
   created () {
@@ -69,6 +107,14 @@ export default {
           )
           this.$router.go(-1)
         }
+      )
+    },
+    addToCart () {
+      let currentItem = this.currentItem
+      let quantity = this.quantity
+      this.$store.commit('cart/addItemToCart', { item: currentItem, quantity: quantity })
+      this.$toasted.success(
+        quantity + ' ' + currentItem.name + ' added to cart'
       )
     }
   }
