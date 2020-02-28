b8b692e2c59e77d4097cd8f165d668e8be97806a
Jimmy Kamau <jimmykambiz@gmail.com>
2020-02-29 00:10:19 +0300

    Remove default redis url

--- api/olduka/settings/base.py
+++ api/olduka/settings/base.py
@@ -245,12 +245,8 @@ SECURE_PROXY_SSL_HEADER = ('HTTP_X_FORWARDED_PROTO', 'https')
 SECURE_SSL_REDIRECT = True
 
 
-CELERY_BROKER_URL = os.environ.get(
-    'REDIS_URL', 'redis://redis:6379'
-)
-CELERY_RESULT_BACKEND = os.environ.get(
-    'REDIS_URL', 'redis://redis:6379'
-)
+CELERY_BROKER_URL = os.environ.get('REDIS_URL')
+CELERY_RESULT_BACKEND = os.environ.get('REDIS_URL')
 CELERY_ACCEPT_CONTENT = ['application/json']
 CELERY_TASK_SERIALIZER = 'json'
 CELERY_RESULT_SERIALIZER = 'json'
