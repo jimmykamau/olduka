79b492162e52fa0f7772f4439bb7eaeea42e1896
Jimmy Kamau <jimmykambiz@gmail.com>
2020-02-29 00:13:02 +0300

    Install celery-beat

--- api/olduka/settings/base.py
+++ api/olduka/settings/base.py
@@ -62,6 +62,7 @@ INSTALLED_APPS = [
     'rest_framework',
     'rest_framework.authtoken',
     'rest_framework_swagger',
+    'django_celery_beat',
 
     'olduka.v1.authentication',
     'olduka.v1.product',
