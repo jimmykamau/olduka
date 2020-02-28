419bac02c6daafaac84704bff803ac45d86a542a
Jimmy Kamau <jimmykambiz@gmail.com>
2020-02-29 00:40:41 +0300

    Fix lint error

--- api/olduka/v1/urls.py
+++ api/olduka/v1/urls.py
@@ -2,10 +2,10 @@ from django.urls import include, path
 from rest_framework_swagger.views import get_swagger_view
 
 app_name = 'v1'
-schema_view = get_swagger_view(title='Olduka')
+SCHEMA_VIEW = get_swagger_view(title='Olduka')
 
 urlpatterns = [
-    path('', schema_view),
+    path('', SCHEMA_VIEW),
     path('auth/', include('olduka.v1.authentication.urls')),
     path('product/', include('olduka.v1.product.urls')),
     path('cart/', include('olduka.v1.cart.urls')),
