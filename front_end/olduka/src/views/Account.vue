05cc9979280f9f4f453f7b9d378cc6c68cad1ee0
Jimmy Kamau <jimmykambiz@gmail.com>
2020-03-06 02:53:35 +0300

    Change tab size

--- front_end/olduka/src/views/Account.vue
+++ front_end/olduka/src/views/Account.vue
@@ -1,15 +1,15 @@
 <template>
-    <div class="uk-width-1-1 uk-margin-remove" uk-height-viewport>
-        <div class="uk-container">
-            <ul uk-tab>
-                <li><a href="#">Profile</a></li>
-            </ul>
+  <div class="uk-width-1-1 uk-margin-remove" uk-height-viewport>
+    <div class="uk-container">
+      <ul uk-tab>
+        <li><a href="#">Profile</a></li>
+      </ul>
 
-            <ul class="uk-switcher uk-margin">
-                <li><Profile /></li>
-            </ul>
-        </div>
+      <ul class="uk-switcher uk-margin">
+        <li><Profile /></li>
+      </ul>
     </div>
+  </div>
 </template>
 
 <script>
