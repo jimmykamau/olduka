533d18566adb283bf0c56d9a3d4741bdd3d925ac
Jimmy Kamau <jimmykambiz@gmail.com>
2020-03-06 03:10:23 +0300

    Create OrderSummary component

---
+++ front_end/olduka/src/components/OrderSummary.vue
@@ -0,0 +1,41 @@
+<template>
+  <div class="uk-container uk-container-small">
+    <div class="uk-card uk-card-default">
+      <div class="uk-card-header">
+        <h3 class="uk-card-title">Order summary</h3>
+      </div>
+      <div class="uk-card-body">
+        <table class="uk-table">
+          <tbody>
+            <tr>
+              <td><b>Subtotal</b></td>
+              <td>KES {{ cartTotal }}</td>
+            </tr>
+            <tr>
+              <td><b>Shipping</b></td>
+              <td>Calculated at next step</td>
+            </tr>
+            <tr class="uk-h4">
+              <td>Order total</td>
+              <td>KES {{ cartTotal }}</td>
+            </tr>
+          </tbody>
+        </table>
+        <button class="uk-button uk-button-primary uk-button-large uk-border-pill">Place order</button>
+      </div>
+    </div>
+  </div>
+</template>
+
+<script>
+import { mapGetters } from 'vuex'
+
+export default {
+  computed: {
+    ...mapGetters('cart', {
+      cart: 'cart',
+      cartTotal: 'cartTotal'
+    })
+  }
+}
+</script>
